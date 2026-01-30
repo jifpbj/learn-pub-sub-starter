4356bbb16444fc807fab97364940d000aa39e68c
Jiff <102629485+jifpbj@users.noreply.github.com>
2026-01-30 10:07:57 -0800

    rabbitmq init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,12 +10,20 @@ import (
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
 	const connectionString = "amqp://guest:guest@localhost:5672/"
-	conn := amqp.Dial(connectionString)
+
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ:", err)
+	}
+
 	defer conn.Close()
 	fmt.Println("Connected to RabbitMQ:", conn.RemoteAddr())
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
+	fmt.Println("Shutting down Peril server...")
+	conn.Close()
 }
