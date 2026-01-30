0388530e8ff4fe024a761e7239c2c81ad12100a7
Jiff <102629485+jifpbj@users.noreply.github.com>
2026-01-29 17:02:04 -0800

    main alts

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const connectionString = "amqp://guest:guest@localhost:5672/"
+	conn := amqp.Dial(connectionString)
+	defer conn.Close()
+	fmt.Println("Connected to RabbitMQ:", conn.RemoteAddr())
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
