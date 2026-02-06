511b9ec598347b9d4a0f709f2123d723b321ab01
Jiff <102629485+jifpbj@users.noreply.github.com>
2026-02-05 17:19:30 -0800

    more setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -28,25 +27,48 @@ func main() {
 		log.Fatalf("Failed to open a channel: %v", err)
 	}
 
-	fmt.Println("Publishing pause message...")
-	err = pubsub.PublishJSON(
-		ch,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	)
-	if err != nil {
-		log.Fatalf("Failed to publish message: %v", err)
-	}
-	if err == nil {
-		fmt.Println("Pause message published successfully.")
-	}
+	gamelogic.PrintServerHelp()
+	var isPaused bool
+	var shouldPublish bool
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("RabbitMQ connection closed. Exiting...")
+	for {
+		shouldPublish = false
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "pause":
+			fmt.Println("Sending Pause Message")
+			isPaused = true
+			shouldPublish = true
+		case "resume":
+			fmt.Println("Sending Resume Message")
+			isPaused = false
+			shouldPublish = true
+		case "quit":
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("Command not understood")
+		}
+		if shouldPublish {
+			fmt.Println("Publishing message...")
+			err = pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: isPaused,
+				},
+			)
+			if err != nil {
+				log.Fatalf("Failed to publish message: %v", err)
+			}
+		}
+	}
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+	// fmt.Println("RabbitMQ connection closed. Exiting...")
 }
